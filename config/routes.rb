f6ad39faf6384c4239940bafc31347e61f707887
Galia Silva <galia.silva@gmail.com>
2015-08-09 12:44:44 -0700

    stopping point mid-way thru checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
+#We'll remove those in favor of a more succinct way of creating Post routes. Call the resources method, and pass it a symbol. By doing this you instruct Rails to create routes for every CRUD action:
+
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/about'
-
-  root to: 'welcome#index'
-
-
+resources :posts
+get 'about' => 'welcome#about'
 end
+
+#Notice that we also removed the get "welcome/index", because we've declared the index view as root. We also cleaned up the about route, by allowing a user to type /about rather than /welcome/about.
