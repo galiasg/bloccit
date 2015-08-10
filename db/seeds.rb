40777bfa628df715051cf9932c62434c469118e2
Galia Silva <galia.silva@gmail.com>
2015-08-09 19:54:30 -0700

    Assignment-Seed-Data-Completed addendum

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,15 @@
 post = Post.where(title: "unique post", body: "the most unique body title in bloccit")
 if post.empty? == false
-  puts "your mom"
+
 else
   Post.create(title: "unique post", body: "the most unique body title in bloccit")
-  puts "your dad"
+
 end
 
 comment = Comment.where(post: post, body: "the most unique comment title in bloccit")
 if comment.empty? == false
-  puts "your mom1"
+
 else
   Comment.create(post: post.last, body: "the most unique commentexit title in bloccit")
-  puts "your dad2"
 end
 
