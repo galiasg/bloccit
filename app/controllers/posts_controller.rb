4a3b2b9ad22434e051b2777ab5875a29185314fb
Galia Silva <galia.silva@gmail.com>
2015-11-12 10:26:53 -0800

    modified post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,16 +10,24 @@ class PostsController < ApplicationController
   def new
    @post = Post.new
   end
-  def create
-     @post = Post.new(params.require(:post).permit(:title, :body))
+   def create
+ # #9
+     @post = Post.new
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
+
+ # #10
      if @post.save
+ # #11
        flash[:notice] = "Post was saved."
        redirect_to @post
      else
+ # #12
        flash[:error] = "There was an error saving the post. Please try again."
        render :new
      end
    end
+
 
   def edit
      @post = Post.find(params[:id])
